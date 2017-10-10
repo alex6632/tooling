4a121cc4f6fba4a7076bb34f93000f0bc5513622
Alexandre Simonin <a.simonin69@gmail.com>
2017-10-10 16:12:00 +0200

    initial commit

---
+++ output/es6.js
@@ -0,0 +1,21 @@
+// mickael@clevermonkey.io
+
+const ul = document.getElementById('jsTodoList');
+const url = '../data/data.json';
+
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+fetch(url).then(resp => resp.json()).then(data => {
+  const items = data.todos;
+  return items.map(item => {
+    const li = createNode('li');
+    li.innerHTML = `${item.label}`;
+    append(ul, li);
+  });
+});
